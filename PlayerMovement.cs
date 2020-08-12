2e994b7eb9ff0ff77a2d12e4dc33d858f0fda319
savestatecorrupted <68752965+savestatecorrupted@users.noreply.github.com>
2020-08-12 17:40:21 -0500

    Updated Player Movement
    Movement runs left to right. Animated running and made it so that he flips based on what direction he runs.

---
+++ PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    //necessary for animations/physics
+    private Rigidbody2D rb2d;
+    private Animator myAnimator;
+
+    private bool facingRight = true;
+
+    //variables to play with
+    public float speed = 2.0f;
+    public float horizMovement;     //horizontal movement. This equals, 1 -1 or 0
+    public float vertMovement;     //vertical movement
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //define the game objects found on player
+        rb2d = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    //Handles input for physics
+    private void Update()
+    {
+        //check direction given by player
+        horizMovement = Input.GetAxis("Horizontal");
+
+    }
+
+    //handles running physics
+    private void FixedUpdate()
+    {
+        //move the character left/right
+        rb2d.velocity = new Vector2(horizMovement * speed, rb2d.velocity.y);
+        myAnimator.SetFloat("speed", Mathf.Abs(horizMovement));
+        Flip(horizMovement);
+        
+    }
+
+    //flip function
+    private void Flip(float horizonal)
+    {
+        if (horizonal < 0 && facingRight || horizonal > 0 && !facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
+ 
---
+++ README.txt
@@ -0,0 +1,1 @@
+Script for my metroidvania game
